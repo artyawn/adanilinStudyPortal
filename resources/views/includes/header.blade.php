e964ffc830759e7bc6330d57ea544a375efac929
artyawn <artem.danilin.1999@gmail.com>
2022-11-30 19:37:26 +0400

    refactor route

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,7 +3,7 @@
                     <li><a href="{{ route('subjects.index') }}" class="nav-link px-2 link-dark">Предметы</a></li>
                     <li><a href="{{ route('groups.index') }}" class="nav-link px-2 link-dark">Группы</a></li>
                     <li><a href="{{ route('users.index') }}" class="nav-link px-2 link-dark">Студенты</a></li>
-                    <li><a href="{{ route('gradeBook.index') }}" class="nav-link px-2 link-dark">Журнал оценок</a></li>
+                    <li><a href="{{ route('gradebook.index') }}" class="nav-link px-2 link-dark">Журнал оценок</a></li>
                 </ul>
                 <div class="col-md-4 text-end">
                         <a class="nav-link px-2 link-dark">user name</a>
