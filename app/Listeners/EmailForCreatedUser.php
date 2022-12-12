cfa55777f4639dee1d7d1d5b32d5cbd1c9a09e67
artyawn <artem.danilin.1999@gmail.com>
2022-12-12 13:16:23 +0400

    creating events and listeners

---
+++ app/Listeners/EmailForCreatedUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserCreated;
+use App\Mail\PasswordMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class EmailForCreatedUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(UserCreated $event)
+    {
+        Mail::to($event->user->email)->send(new PasswordMail($event->password));
+    }
+}
